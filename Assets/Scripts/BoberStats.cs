f7970e4327459338bbc08c77fb93c5d77b71b787
Auri <akraszczynska0@gmail.com>
2024-10-28 22:28:01 +0100

    adding sounds

--- Assets/Scripts/BoberStats.cs
+++ Assets/Scripts/BoberStats.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 public class BoberStats : MonoBehaviour
 {
     public int moneyGain;
-
     public GameObject nextBober;
-
     public int boberLevel;
 
-    // Update is called once per frame
+    [SerializeField] private AudioClip startSound; // Dodaj dŸwiêk na starcie
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
     void Start()
     {
+        // Dodaj komponent AudioSource i przypisz dŸwiêk
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = startSound;
+
+        // Odtwórz dŸwiêk, jeœli jest przypisany
+        if (audioSource && startSound)
+        {
+            audioSource.PlayOneShot(startSound);
+        }
+
+        // Wywo³aj event MoneyGainChange
         EventSystem.current.MoneyGainChange(moneyGain);
     }
-    
 }
+
