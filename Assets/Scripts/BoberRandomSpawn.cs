7abf536e20827497f69d3e120314dc94c0705edf
Auri <akraszczynska0@gmail.com>
2024-10-28 21:12:22 +0100

    Adding sounds

--- Assets/Scripts/BoberRandomSpawn.cs
+++ Assets/Scripts/BoberRandomSpawn.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,27 +7,40 @@ public class BoberRandomSpawn : MonoBehaviour
 {
     [SerializeField] private Transform leftCornerX, rightCornerX;
     [SerializeField] private Transform cornerUp, cornerDown;
-
-    private float randomX,randomY;
-
     [SerializeField] private GameObject spawnCrate;
 
+    [SerializeField] private AudioClip spawnSound; // Dodaj pole AudioClip do przypisania dŸwiêku.
+    private AudioSource audioSource; // Zmienna dla AudioSource.
+
     public SO_Int crateSpawnTimer;
+
+    private float randomX, randomY;
 
     private void Start()
     {
         crateSpawnTimer.soInt = 10;
+        audioSource = gameObject.AddComponent<AudioSource>(); // Dodanie komponentu AudioSource.
+        audioSource.clip = spawnSound; // Przypisz dŸwiêk do AudioSource.
+
         StartCoroutine(SpawnRandom());
     }
 
     IEnumerator SpawnRandom()
     {
         yield return new WaitForSeconds(crateSpawnTimer.soInt);
-        randomX = Random.Range(leftCornerX.position.x,rightCornerX.position.x);
+        randomX = Random.Range(leftCornerX.position.x, rightCornerX.position.x);
         randomY = Random.Range(cornerDown.position.y, cornerUp.position.y);
-        new Vector3(randomX, randomY, 0);
-        Instantiate(spawnCrate, new Vector3(randomX, randomY, 0), Quaternion.Euler(-270, -90, 90));
+        Vector3 spawnPosition = new Vector3(randomX, randomY, 0);
+
+        Instantiate(spawnCrate, spawnPosition, Quaternion.Euler(-270, -90, 90));
+
+        // Odtwórz dŸwiêk przy ka¿dym spawnie
+        if (audioSource && spawnSound)
+        {
+            audioSource.PlayOneShot(spawnSound);
+        }
+
         yield return StartCoroutine(SpawnRandom());
     }
-
 }
+
--- Assets/ShopController.cs
+++ Assets/ShopController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -6,30 +5,45 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour
 {
-   [SerializeField] SO_Int PlayerMoney;
-   
-   [Header("Spawn Upgrade")]
-   [SerializeField] SO_Int crateSpawnTimer;
-   public int upgradeLimit;
-   int spawnbUpgradeAlready;
-   [SerializeField] int moneyNeeded;
-   [SerializeField] TextMeshProUGUI buttonTextSpawn;
-   
-   void Start()
-   {
-      buttonTextSpawn.text = moneyNeeded.ToString();
-   }
+    [SerializeField] SO_Int PlayerMoney;
 
+    [Header("Spawn Upgrade")]
+    [SerializeField] SO_Int crateSpawnTimer;
+    public int upgradeLimit;
+    int spawnbUpgradeAlready;
+    [SerializeField] int moneyNeeded;
+    [SerializeField] TextMeshProUGUI buttonTextSpawn;
 
+    [Header("Sound Settings")]
+    [SerializeField] private AudioClip upgradeSound; // Pole na dŸwiêk przy przycisku
+    private AudioSource audioSource;
 
-   public void UpgradeBoberSpawn()
-   {
-      
-      if (spawnbUpgradeAlready != upgradeLimit && PlayerMoney.soInt > moneyNeeded)
-      {
-         moneyNeeded = moneyNeeded * 2;
-         spawnbUpgradeAlready += 1;
-         crateSpawnTimer.soInt -= 1;
-      }
-   }
+    void Start()
+    {
+        buttonTextSpawn.text = moneyNeeded.ToString();
+
+        // Dodaj AudioSource do obiektu, jeœli jeszcze go nie ma
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = upgradeSound;
+    }
+
+    public void UpgradeBoberSpawn()
+    {
+        if (spawnbUpgradeAlready != upgradeLimit && PlayerMoney.soInt > moneyNeeded)
+        {
+            // Ulepszenie
+            moneyNeeded *= 2;
+            spawnbUpgradeAlready += 1;
+            crateSpawnTimer.soInt -= 1;
+
+            // Zaktualizuj tekst przycisku
+            buttonTextSpawn.text = moneyNeeded.ToString();
+
+            // Odtwórz dŸwiêk
+            if (audioSource && upgradeSound)
+            {
+                audioSource.PlayOneShot(upgradeSound);
+            }
+        }
+    }
 }
